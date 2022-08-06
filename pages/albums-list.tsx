6a751717f308d2cae7ca62c47e3d40bb371f38b7
Frank Corkery <fcorkery@Franks-MBP.lan>
2022-08-06 14:43:24 -0400

    more typescript errors

--- pages/albums-list.tsx
+++ pages/albums-list.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react'
-import type { NextPage } from 'next';
 import Link from '../src/Link';
 import ProTip from '../src/ProTip';
 import Copyright from '../src/Copyright';
