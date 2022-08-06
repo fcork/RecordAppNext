f68ca70b8f9554ada5082e5861c9c3f3b2f2d6bf
fcork <frankcorkery@gmail.com>
2022-08-06 17:45:49 -0400

    Add prod on phone
    Attempting env prod connect

--- pages/albums-list.tsx
+++ pages/albums-list.tsx
@@ -12,7 +12,11 @@ import ListView from '../src/components/ListView';
 
 
 export async function getServerSideProps() {
-  const res = await fetch(`http://localhost:3004/api/albums`)
+
+  let dev = process.env.NODE_ENV !== 'production';
+  let { DEV_URL, PROD_URL } = process.env;
+
+  const res = await fetch(`${dev ? DEV_URL : PROD_URL}/api/albums`);
   const newData = await res.json()
   return {
     props: {
